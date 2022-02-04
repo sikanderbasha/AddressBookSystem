4c1dcfc17d4fae786786ef2c8b8abe2f3f7b168d
Sikander <sikku.rk405@gmail.com>
2022-02-04 21:05:35 +0530

    [Sikander] Added Edit contact program

--- AddressBook/AddressBook/Address.cs
+++ AddressBook/AddressBook/Address.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class Address
+    public class Address
     {
         List<Contacts> address = new List<Contacts>();
-
+        Contacts contact = new Contacts();
         public void AddContact()
         {
 
@@ -38,7 +38,7 @@ namespace AddressBook
             System.Console.WriteLine("Enter Email-Id: ");
             contacts.Email = Console.ReadLine();
             Console.WriteLine("Contact details of : " + contacts.FirstName);
-            Console.WriteLine("FirstName : " + contacts.FirstName 
+            Console.WriteLine("FirstName : " + contacts.FirstName
                             + "\n" + "LastName : " + contacts.LastName
                             + "\n" + "Address : " + contacts.Address
                             + "\n" + "City : " + contacts.City
@@ -48,7 +48,79 @@ namespace AddressBook
                             + "\n" + "Email : " + contacts.Email);
 
             address.Add(contacts);
-           
+
+        }
+
+        public void EditContact(string FirstName)
+        {       
+            foreach (var data in address)
+            {
+                if (data.FirstName == FirstName)
+                {
+                    contact = data;
+                    Console.WriteLine("\n 1. First name\n 2. Last name\n 3. Address\n 4. City\n 5. State\n 6. Zipcode\n 7. Phone number\n 8. Email ID\n 9. Exit");                   
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                Console.WriteLine("Enter the First name");
+                                contact.FirstName = Convert.ToString(Console.ReadLine());
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter the Last name");
+                                contact.LastName = Convert.ToString(Console.ReadLine());
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter the Address");
+                                contact.Address = Convert.ToString(Console.ReadLine());
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter the City");
+                                contact.City = Convert.ToString(Console.ReadLine());
+                                break;
+                            case 5:
+                                Console.WriteLine("Enter the State");
+                                contact.State = Convert.ToString(Console.ReadLine());
+                                break;
+                            case 6:
+                                Console.WriteLine("Enter the Zip Code");
+                                contact.Zip = Convert.ToInt32(Console.ReadLine());
+                                break;
+                            case 7:
+                                Console.WriteLine("Enter the Phone Number");
+                                contact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                                break;
+                            case 8:
+                                Console.WriteLine("Enter the Email");
+                                contact.Email = Convert.ToString(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("Enter Correct option!");
+                                break;
+                        }
+                        Display();
+                        return;
+                    
+
+                }
+            }
+        }
+
+
+        public void Display()
+        {
+            foreach (var data in address)
+            {
+                Console.WriteLine("Updated contact details of : " + data.FirstName);
+                Console.WriteLine("FirstName : " + data.FirstName
+                            + "\n" + "LastName : " + data.LastName
+                            + "\n" + "Address : " + data.Address
+                            + "\n" + "City : " + data.City
+                            + "\n" + "State : " + data.State
+                            + "\n" + "Zip : " + data.Zip
+                            + "\n" + "PhoneNumber : " + data.PhoneNumber
+                            + "\n" + "Email : " + data.Email);
+            }
         }
     }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -10,10 +10,34 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Welcome to Address Book System!");
-
+            Console.WriteLine("Welcome to Address Book System!");
             Address address = new Address();
-            address.AddContact();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n 1.Add Contact."
+                                + "\n 2.Edit Contact."
+                                + "\n 3.Exit.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        address.AddContact();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the First Name To Edit Contact : ");
+                        string firstName = Convert.ToString(Console.ReadLine());
+                        address.EditContact(firstName);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter Correct Option!!!");
+                        break;
+                }
+            }
+
             Console.ReadKey();
         }
     }
